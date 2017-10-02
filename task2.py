d5c8d012c30065f8eff4d9f6942a16a090874dec
ankit07829709 <Ankit.sharma@dal.ca>
2017-10-02 15:51:58 +0000

    edit

--- task2.py
+++ task2.py
@@ -1,2 +1,9 @@
-def crime():
- my_file=open(crime.csv)
+import csv
+ with open(file,"crime.csv") as myfile:
+    reader=csv.reader(a)
+      w=[]
+     for row in reader:
+       w.extend(row)
+      
+       
+print (file)
